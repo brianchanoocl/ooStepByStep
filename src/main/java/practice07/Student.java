810ce902454b09eec187192be673585a31bee28e
brianchanoocl <Delubdi118>
2021-12-01 18:49:20 +0800

    [ooStepByStep] [Brian] -feat Practice 8 dun know ID

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,25 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass){
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public int getNumber() {
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        String response = super.introduce() + " I am a Student.";
+        if(klass.getLeader() == this)
+            response += String.format(" I am Leader of Class %d.", klass.getNumber());
+        return response;
+    }
 }
