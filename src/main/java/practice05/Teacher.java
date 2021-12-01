5ffd67dac5681dcd1e7f531ab6f1339bbc2de96e
brianchanoocl <Delubdi118>
2021-12-01 17:52:17 +0800

    [ooStepByStep] [Brian] -feat complete practice 05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,30 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private String klass;
+
+    public Teacher(String name, int age){
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Teacher(String name, int age, int klass){
+        super(name, age);
+            this.klass = String.valueOf(klass);
+    }
+
+    public int getKlass() {
+        if (klass != null)
+            return Integer.parseInt(klass);
+        else
+            return -1;
+    }
+
+    public String introduce() {
+        if(getKlass() >= 0)
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %d.", getName(), getAge(), getKlass());
+        else
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.", getName(), getAge());
+
+    }
 }
